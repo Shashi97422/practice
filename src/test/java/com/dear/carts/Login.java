291341fb43482e9b9043aa69fcb9b4c96e5c995c
HP <HP@Shashidharswamy>
2022-12-21 11:18:59 +0530

    test

---
+++ src/test/java/com/dear/carts/Login.java
@@ -0,0 +1,14 @@
+package com.dear.carts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Login {
+public static void main(String[] args) {
+	WebDriverManager.chromedriver().setup();
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://www.facebook.com/");
+}
+}
